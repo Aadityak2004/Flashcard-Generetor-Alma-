d352ea2733d80f4f3f91f7634684839ddde12d37
Aadityak12 <adityakathoke15@gmail.com>
2023-08-02 14:11:28 +0530

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+//About ME Function
+function About() {
+    let About_Me = document.getElementById("About_text")
+    if (About_Me.style.display != "block") {
+        About_Me.style.display = "block"
+    }
+    else {
+        About_Me.style.display = "none"
+    }
+}
+
+//Contact Function
+function Contact() {
+    let Contact_Me = document.getElementById("Contact_text")
+    if (Contact_Me.style.display != "block") {
+        Contact_Me.style.display = "block"
+    }
+    else {
+        Contact_Me.style.display = "none"
+    }
+}
+
+//Education Function
+function Education() {
+    let Education_Me = document.getElementById("Education_text")
+    if (Education_Me.style.display != "block") {
+        Education_Me.style.display = "block"
+    }
+    else {
+        Education_Me.style.display = "none"
+    }
+}
